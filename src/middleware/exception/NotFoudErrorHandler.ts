2ee29d16e881618cd5da4f9daabbe3f13f4adb4d
KDM_korea <kdm_korea@naver.com>
2021-01-12 15:11:02 +0900

    Feat: Notfound error Handling

---
+++ src/middleware/exception/NotFoudErrorHandler.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+import { NotFoundError } from '../../lib/expeption';
+
+const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+	throw new NotFoundError('존재하지 않는 API입니다.');
+};
+
+export default notFoundHandler;
