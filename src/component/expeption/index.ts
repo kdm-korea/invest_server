e90d71ad5e1a76e5db36fe2cd29201abe02ded44
KDM_korea <kdm_korea@naver.com>
2021-01-14 16:32:16 +0900

    Feat: 에러 인덱스 추가

---
+++ src/component/expeption/index.ts
@@ -0,0 +1,5 @@
+export { default as BaseError } from './BaseError';
+export { default as NotFoundError } from './NotFoundError';
+export { default as BadRequestError } from './BadRequestError';
+export { default as ForbiddenError } from './ForbiddenError';
+export { default as UnauthorizedError } from './UnauthorizedError';
