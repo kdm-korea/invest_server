9227d0f5557b3114ec86cc353e183379707224b0
KDM_korea <kdm_korea@naver.com>
2021-01-14 16:39:38 +0900

    Fix: 배열 수정

--- src/util/createUUID.ts
+++ src/util/createUUID.ts
@@ -2,5 +2,5 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function createUUID(): string {
 	const uuid = uuidv4().split('-');
-	return uuid[3] + uuid[2] + uuid[1] + uuid[4] + uuid[5];
+	return uuid[2] + uuid[1] + uuid[0] + uuid[3] + uuid[4];
 }
