0a425e8b834f55523762185e96718c4aaeadf590
KDM_korea <kdm_korea@naver.com>
2021-01-25 17:40:42 +0900

    Fix: exception class 폴더 이동으로 인해 수정됨

--- src/middleware/exception/NotFoudErrorHandler.ts
+++ src/middleware/exception/NotFoudErrorHandler.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { NotFoundError } from '../../component/expeption';
+import { NotFoundError } from '../../util/expeption';
 
 const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
 	throw new NotFoundError('존재하지 않는 API입니다.');
