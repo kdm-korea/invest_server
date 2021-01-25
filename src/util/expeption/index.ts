489b73bd93a165a938722c41b49008789ff1e193
KDM_korea <kdm_korea@naver.com>
2021-01-25 17:30:55 +0900

    Fix: 폴더 위치를 유틸로 변경

--- src/util/expeption/index.ts
+++ src/util/expeption/index.ts
@@ -3,3 +3,4 @@ export { default as NotFoundError } from './NotFoundError';
 export { default as BadRequestError } from './BadRequestError';
 export { default as ForbiddenError } from './ForbiddenError';
 export { default as UnauthorizedError } from './UnauthorizedError';
+export { default as ValidationError } from './ValidationError';
